a0e198306322f9619109f0e8ce7f4b3017110574
pyxxelated <quipot.erickajane@gmail.com>
2021-01-11 20:10:44 +0800

    Adding Header and Bottom Tabs

--- App.js
+++ App.js
@@ -1,68 +1,87 @@
-// In App.js in a new project
 import * as React from 'react';
-import { Button,View, Text } from 'react-native';
+import { Button, Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
-function HomeScreen({ navigation }) {
+// function DetailsScreen() {
+//   return (
+//     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+//       <Text>Details!</Text>
+//     </View>
+//   );
+// }
+
+function HomeScreen() {
   return (
-    <View style={{ 
-          flex: 1, 
-          alignItems: 'center', 
-          justifyContent: 'center' 
-          }}>
-      <Text>Home Screen</Text>
-      <Button
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Home screen</Text>
+      {/* <Button
         title="Go to Details"
         onPress={() => navigation.navigate('Details')}
-      />
+      /> */}
     </View>
   );
 }
 
-function DetailsScreen({navigation}) {
+function ControlScreen() {
   return (
-    <View style={{ 
-          flex: 1, 
-          alignItems: 'center', 
-          justifyContent: 'center' 
-          }}>
-      <Text>Details Screen</Text>
-      <Button
-        title="Go to About Us"
-        onPress={() => navigation.navigate('AboutUs')}
-      />
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Control screen</Text>
     </View>
   );
 }
-function AboutUs({navigation}) {
+function DashboardScreen() {
   return (
-    <View style={{ 
-          flex: 1, 
-          alignItems: 'center', 
-          justifyContent: 'center' 
-          }}>
-      <Text>Details Screen</Text>
-      <Button
-        title="Go to About us... again"
-        onPress={() => navigation.push('AboutUs')}
-      />
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Dashboard</Text>
     </View>
   );
 }
 
-const Stack = createStackNavigator();
+const HomeStack = createStackNavigator();
 
-function App() {
+function HomeStackScreen() {
+  return (
+    <HomeStack.Navigator>
+      <HomeStack.Screen name="Home" component={HomeScreen} />
+      {/* <HomeStack.Screen name="Details" component={DetailsScreen} /> */}
+    </HomeStack.Navigator>
+  );
+}
+
+const ControlStack = createStackNavigator();
+
+function ControlStackScreen() {
+  return (
+    <ControlStack.Navigator>
+      <ControlStack.Screen name="Control" component={ControlScreen} />
+      {/* <SettingsStack.Screen name="Details" component={DetailsScreen} /> */}
+    </ControlStack.Navigator>
+  );
+}
+
+const DashboardStack = createStackNavigator();
+
+function DashboardStackScreen() {
+  return (
+    <DashboardStack.Navigator>
+      <DashboardStack.Screen name="Dashboard" component={DashboardScreen} />
+      {/* <SettingsStack.Screen name="Details" component={DetailsScreen} /> */}
+    </DashboardStack.Navigator>
+  );
+}
+
+const Tab = createBottomTabNavigator();
+
+export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Home">
-        <Stack.Screen name="Home" component={HomeScreen}/>
-        <Stack.Screen name="Details" component={DetailsScreen} />
-        <Stack.Screen name="AboutUs" component={AboutUs} />
-      </Stack.Navigator>
+      <Tab.Navigator>
+        <Tab.Screen name="Home" component={HomeStackScreen} />
+        <Tab.Screen name="Control" component={ControlStackScreen} />
+        <Tab.Screen name="Dashboard" component={DashboardStackScreen}/>
+      </Tab.Navigator>
     </NavigationContainer>
   );
 }
-
-export default App;
